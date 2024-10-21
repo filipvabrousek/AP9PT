3b5976fa3277860abcb5e3bbe27f512c178deca8
Filip Vabrousek <filipvabrousek1@gmail.com>
2024-10-21 17:47:40 +0200

    Add files via upload

---
+++ 2024/prez/21-10-24-sk-2/Modifiers-21-01-24/Modifiers_21_01_24App.swift
@@ -0,0 +1,17 @@
+//
+//  Modifiers_21_01_24App.swift
+//  Modifiers-21-01-24
+//
+//  Created by student on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Modifiers_21_01_24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
