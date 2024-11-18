177b645130da3e5c19f389dccfe43e9729d17f53
Filip Vabrousek <filipvabrousek1@gmail.com>
2024-11-18 17:53:33 +0100

    Add files via upload

---
+++ 2024/prez/18-11-24/combineFrameworks/CombineFrameworks/CombineFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFrameworksApp.swift
+//  CombineFrameworks
+//
+//  Created by student on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
