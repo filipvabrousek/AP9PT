0f28214110c77b4053fb8e32185a6f578f2c4bdf
Filip Vabrousek <filipvabrousek1@gmail.com>
2024-11-25 11:53:44 +0100

    Add files via upload

---
+++ 2024/prez/25-11-24-sk-1/SwiftUIinUIKit/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SwiftUIinUIKit
+//
+//  Created by student on 19.11.2024.
+//
+
+import SwiftUI
+import Foundation
+
+// UIKit in SwiftUI
+
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024/prez/25-11-24-sk-1/SwiftUIinUIKit/SwiftUIinUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinUIKitApp.swift
+//  SwiftUIinUIKit
+//
+//  Created by student on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIinUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
