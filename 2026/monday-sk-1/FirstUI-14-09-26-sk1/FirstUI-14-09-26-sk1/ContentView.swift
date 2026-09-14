ed38e7c67d157e7491ad2702a940cc78e533940d
Filip Vabrousek <filipvabrousek1@gmail.com>
2026-09-14 15:55:31 +0200

    Add files via upload

---
+++ 2026/monday-sk-1/FirstUI-14-09-26-sk1/FirstUI-14-09-26-sk1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstUI-14-09-26-sk1
+//
+//  Created by Filip Vabroušek on 14.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2026/monday-sk-1/FirstUI-14-09-26-sk1/FirstUI-14-09-26-sk1/FirstUI_14_09_26_sk1App.swift
@@ -0,0 +1,18 @@
+//
+//  FirstUI_14_09_26_sk1App.swift
+//  FirstUI-14-09-26-sk1
+//
+//  Created by Filip Vabroušek on 14.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct FirstUI_14_09_26_sk1App: App {
+    var body: some Scene {
+        WindowGroup {
+           Text("Hello")
+                .foregroundStyle(.green)
+        }
+    }
+}
